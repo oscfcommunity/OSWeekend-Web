fe7f2d4b9fdc7e37826012c785fd7989598a5562
neelshah2409 <shahneel2409@gmail.com>
2023-10-01 01:00:54 +0530

    Frontend Added

--- OSW-backend/Routes/ContactUsRoutes.js
+++
@@ -1,7 +0,0 @@
-const { ContactUs } = require("../Controller/ContactController");
-
-const contactUsRoutes = (app) => {
-  app.post("/user/contactus", ContactUs);
-};
-
-module.exports = contactUsRoutes;
